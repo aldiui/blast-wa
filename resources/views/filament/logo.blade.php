a68b8fae7426212110f146732d2b8d5ac1f83f5a
aldiui <aldijaya280902@gmail.com>
2024-08-27 22:32:26 +0700

    update data

--- resources/views/filament/logo.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="flex items-center justify-center gap-x-3 dark:bg-gray-800">
-    <img src="" class="h-10 rounded-md" alt="Logo">
-    <div class="whitespace-nowrap">
-        <div class="text-xs font-semibold text-gray-600 dark:text-white">WA Blast</div>
-        <div class="text-xs font-semibold text-gray-400 dark:text-white">{{ config('app.name') }}</div>
-    </div>
-</div>
--- resources/views/filament/pages/pengaturan-page.blade.php
+++
@@ -1,3 +0,0 @@
-{{-- resources/views/filament/pages/pengaturan-page.blade.php --}}
-<x-filament::page>
-</x-filament::page>
