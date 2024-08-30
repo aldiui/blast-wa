af2c5a5d2bcc0c3da119e6573e9d2964f77e0014
arizainalf <arizainalf@gmail.com>
2024-08-30 21:33:19 +0700

    new app

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
-use App\Models\Kelas;
-use App\Models\Pengaturan;
 use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,41 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => '11221122',
-        ]);
+        // User::factory(10)->create();
 
-        $kelasData = [
-            ["nama" => "1 A"],
-            ["nama" => "1 B"],
-            ["nama" => "1 C"],
-            ["nama" => "2 A"],
-            ["nama" => "2 B"],
-            ["nama" => "2 C"],
-            ["nama" => "3 A"],
-            ["nama" => "3 B"],
-            ["nama" => "3 C"],
-            ["nama" => "4 A"],
-            ["nama" => "4 B"],
-            ["nama" => "4 C"],
-            ["nama" => "5 A"],
-            ["nama" => "5 B"],
-            ["nama" => "5 C"],
-            ["nama" => "6 A"],
-            ["nama" => "6 B"],
-            ["nama" => "6 C"],
-        ];
-
-        Kelas::insert($kelasData);
-
-        Pengaturan::create([
-            'nama' => 'MI Condong',
-            'logo' => 'logo.png',
-            'email' => 'micondong@gmail.com',
-            'no_telepon' => '085710000000',
-            'alamat' => 'CirebonKomplek Pesantren Condong 46196 Tasikmalaya Jawa Barat',
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,7 @@
 <?php
 
-use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\KelasController;
-use App\Http\Controllers\Admin\PengaturanController;
-use App\Http\Controllers\Admin\PengumumanController;
-use App\Http\Controllers\Admin\SiswaController;
-use App\Http\Controllers\Admin\WhatsappController;
-use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['guest'])->group(function () {
-    Route::match(['get', 'post'], '/login', [AuthController::class, 'login'])->name('login');
-});
-
-Route::middleware(['auth'])->group(function () {
-    Route::resource('/', DashboardController::class)->names('dashboard');
-    Route::post('/siswa/import', [SiswaController::class, 'import'])->name('siswa.import');
-    Route::resource('/siswa', SiswaController::class)->names('siswa');
-    Route::resource('/kelas', KelasController::class)->names('kelas');
-    Route::resource('/pengumuman', PengumumanController::class)->names('pengumuman');
-    Route::get('/logout', [AuthController::class, 'logout']);
-    Route::match(['get', 'put'], '/pengaturan', [PengaturanController::class, 'index'])->name('pengaturan');
-    Route::match(['get', 'post'], '/whatsapp', [WhatsappController::class, 'index'])->name('whatsapp');
-
+Route::get('/', function () {
+    return view('welcome');
 });
